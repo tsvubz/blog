014ca06050ae00ab9512e12e11eeb30e225f1ddd
Terence Svubure <tsvubure@gmail.com>
2025-05-29 12:52:47 +0200

    form errors

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def register():
     form = RegistrationForm()
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)
